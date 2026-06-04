fd57e98dfa69c4010e500c08205fb96546583c3a
dorisevangeline-web <dorisevangeline@gmail.com>
2026-06-04 10:08:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ FILTER_KEYWORDS = ["icon", "logo", "arrow", "btn", "button", "footer", "header",
 
 def create_driver():
     chrome_options = Options()
+
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    
+    service = Service(ChromeDriverManager().install())
+    
+    # 現在這裡就可以正確使用 service 變數了
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
